7bf7735a24b29ad4e28003d683e79421e92dfbee
msuhels <m.suhel11@gmail.com>
2021-12-23 15:07:55 +0530

    crud and authentication using passport

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', [AuthController::class, 'details']);
 });
+Route::apiResource('projects', ProjectController::class)->middleware('auth:api');
